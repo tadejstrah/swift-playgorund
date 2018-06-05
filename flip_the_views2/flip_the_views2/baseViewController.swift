eae6d1e4c20de034b4277e37368581e116c2d336
Tadej Strah <tadej.strah@gmail.com>
2018-06-05 21:07:01 +0200

    meh

--- flip_the_views2/flip_the_views2/baseViewController.swift
+++ flip_the_views2/flip_the_views2/baseViewController.swift
@@ -28,15 +28,37 @@ class baseViewController: UIViewController {
     var currentState:animationState!
     var animator:UIViewPropertyAnimator?
     var panGestureRecognizer: UIPanGestureRecognizer!
+    var didChangeZIndexes: Bool = false
+    
     var animationFraction: CGFloat = 0{
         didSet{
             statusLabel.text = animationFraction.description
+            if animationFraction > 1/2, !didChangeZIndexes {
+                if backSideView.layer.zPosition == 1{
+                    backSideView.layer.zPosition = 0
+                }
+                else{
+                    backSideView.layer.zPosition = 1
+                }
+                if frontSideView.layer.zPosition == 1{
+                    frontSideView.layer.zPosition = 1
+                }
+                else{
+                    frontSideView.layer.zPosition = 0
+                }
+            didChangeZIndexes = true
+            }
         }
     }
     
     @IBOutlet weak var frontSideView: UIView!
     @IBOutlet weak var backSideView: UIView!
-    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!{
+        didSet{
+            statusLabel.layer.zPosition = 3
+        }
+    }
+    
     @IBOutlet var baseView: UIView!
     
     var rotationWay: Double = 1.0
@@ -55,6 +77,10 @@ class baseViewController: UIViewController {
         default: return 1
         }
     }
+    
+    var frontSideViewZIndex: CGFloat = 0
+    var backSideViewZIndex: CGFloat = 0
+    
     
     func perform3DTransforms(){
         var frontSideViewTransform = CATransform3DIdentity
@@ -77,14 +103,13 @@ class baseViewController: UIViewController {
     @objc func flipViews(detectedBy recognizer: UIPanGestureRecognizer){
         guard recognizer.view != nil else {return}
         let translation = recognizer.translation(in: self.view.superview)
-        
+        let velocity = recognizer.velocity(in: self.view.superview)
         switch recognizer.state {
         case .began:
             startPanning()
         case .changed:
-            doTheFlipping(translation: translation )
+            doTheFlipping(translation: translation, velocity: velocity )
         case .ended:
-            let velocity = recognizer.velocity(in: self.view.superview)
             endAnimation(translation: translation, velocity: velocity)
         default:
             return
@@ -99,16 +124,31 @@ class baseViewController: UIViewController {
         animationFraction = animator!.fractionComplete
     }
     
-    func doTheFlipping(translation: CGPoint){
+    func doTheFlipping(translation: CGPoint, velocity:CGPoint){
         if let animator = self.animator {
             var progress:CGFloat = 0
             progress = abs(translation.x / self.view.frame.width * 2)
-            if (translation.x < 0){
-                rotationWay = -1.0
-            }
-            else{
-                rotationWay = 1.0
-            }
+            switch currentState{
+            case .settingsViewOpened:
+                print("settings view opened, translation: \(translation.x)")
+                if translation.x < 0{
+                    animator.isReversed = false
+                }
+                else{
+                    animator.isReversed = true
+                    }
+            case .unlockViewOpened:
+                print("unlock view opened, translation: \(translation.x)")
+                if translation.x < 0{
+                    animator.isReversed = true
+                }
+                else{
+                    animator.isReversed = false
+                }
+            default: animator.isReversed = true
+
+        }
+            print(animator.isReversed)
             animator.fractionComplete = progress
             animationFraction = animator.fractionComplete
         }
@@ -123,28 +163,47 @@ class baseViewController: UIViewController {
             
             animationFraction = animator.fractionComplete
             
-            switch currentState{
-            case .settingsViewOpened:
-                    perform3DTransforms()
-                    panGestureRecognizer.isEnabled = true
+            switch currentState {
             case .unlockViewOpened:
-                    perform3DTransforms()
-                    panGestureRecognizer.isEnabled = true
+                if animationFraction >= 1 / 2 {
+                    animator.isReversed = false
+                    animator.addCompletion ({ (position: UIViewAnimatingPosition) in
+                        self.currentState = .settingsViewOpened
+                        self.panGestureRecognizer.isEnabled = true
+                    })
+                } else {
+                    animator.isReversed = true
+                    animator.addCompletion ({ (position: UIViewAnimatingPosition) in
+                        self.currentState = .unlockViewOpened
+                        self.panGestureRecognizer.isEnabled = true
+                    })
+                animator.isReversed = false
+                }
+            case .settingsViewOpened:
+                if animationFraction >= 1 / 2  {
+                    animator.isReversed = false
+                    animator.addCompletion ({ (position: UIViewAnimatingPosition) in
+                        self.currentState = .unlockViewOpened
+                        self.panGestureRecognizer.isEnabled = true
+                    })
+                } else {
+                    animator.isReversed = true
+                    animator.addCompletion ({ (position: UIViewAnimatingPosition) in
+                        self.currentState = .settingsViewOpened
+                        self.panGestureRecognizer.isEnabled = true
+                    })
+                animator.isReversed = true
+                }
             default:
-                print("wuht??")
+                print("Unknown state")
             }
-            
+
             let vector = CGVector(dx: velocity.x / 100, dy: velocity.y / 100)
             let spgingParameters = UISpringTimingParameters(dampingRatio: 0.5, initialVelocity: vector)
             animator.continueAnimation(withTimingParameters: spgingParameters, durationFactor: 1)
-            currentState.changeState()
-
+            print("neki \(animator.isReversed)")
+            didChangeZIndexes = false
         }
     }
-    
-
-    
-    
-    
 }
 
