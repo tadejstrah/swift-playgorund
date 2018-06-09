164ca2f929cff0e118fb35b8432967528503be8a
Tadej Strah <tadej.strah@gmail.com>
2018-06-09 22:33:29 +0200

    ads

--- flip_the_views2/flip_the_views2/baseViewController.swift
+++ flip_the_views2/flip_the_views2/baseViewController.swift
@@ -48,7 +48,9 @@ class baseViewController: UIViewController {
                 }
             didChangeZIndexes = true
             }
-        }
+
+			
+		}
     }
     
     @IBOutlet weak var frontSideView: UIView!
